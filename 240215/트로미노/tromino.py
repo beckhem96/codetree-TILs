e0a5aeeff3a42749d6f35942333131381219205e
임완택 <57139287+beckhem96@users.noreply.github.com>
2024-02-15 23:32:16 +0900

    Title: 트로미노, Time: 278ms, Memory: 26MB, Status: Passed - CodeTree

--- 240215/트로미노/tromino.py
+++ 240215/트로미노/tromino.py
@@ -1,11 +1,19 @@
+n, m = map(int, input().split())
+array = [list(map(int, input().split())) for _ in range(n)]
+loc = [[-1, 0],[0, 1], [1, 0], [0, -1]]
+result = 0
+temp = 0
+
 # 기준은 중심
 def check_one(si, sj):
     ans = 0
     temp = [[0, 1], [1, 2], [2, 3], [3, 0]] # 중심 제외 위치값 
     for a, b in temp:
         max_value = 0
-        si1, sj1 = loc[a]
-        si2, sj2 = loc[b]
+        ti1, tj1 = loc[a]
+        ti2, tj2 = loc[b]
+        si1, sj1 = si + ti1, sj + tj1
+        si2, sj2 = si + ti2, sj + tj2
         if (check_line(si1, sj1) and check_line(si2, sj2)):
             max_value = array[si][sj] + array[si1][sj1] + array[si2][sj2]
 
@@ -17,8 +25,11 @@ def check_two(si, sj):
     temp = [[0, 2], [1, 3]] # 중심 제외 위치값 
     for a, b in temp:
         max_value = 0
-        si1, sj1 = loc[a]
-        si2, sj2 = loc[b]
+        ti1, tj1 = loc[a]
+        ti2, tj2 = loc[b]
+        si1, sj1 = si + ti1, sj + tj1
+        si2, sj2 = si + ti2, sj + tj2
+
         if (check_line(si1, sj1) and check_line(si2, sj2)):
             max_value = array[si][sj] + array[si1][sj1] + array[si2][sj2]
 
@@ -31,12 +42,6 @@ def check_line(li, lj):
     else: 
         return False
 
-
-n, m = map(int, input().split())
-array = [list(map(int, input().split())) for _ in range(n)]
-loc = [[-1, 0],[0, 1], [1, 0], [0, -1]]
-result = 0
-temp = 0
 for i in range(n):
     for j in range(m):
         temp = max(check_one(i, j), check_two(i, j))
