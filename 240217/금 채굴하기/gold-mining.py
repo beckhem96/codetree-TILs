c8dc51b12ac97da82bc04dd11f975b637d02ed36
임완택 <57139287+beckhem96@users.noreply.github.com>
2024-02-17 17:40:53 +0900

    Title: 금 채굴하기, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240217/금 채굴하기/gold-mining.py
+++ 240217/금 채굴하기/gold-mining.py
@@ -3,28 +3,27 @@ def cost(K):
 
 def calArea(K, ti, tj, n):
     cnt = 0
-    if (K == 0):
-        if (arr[ti][tj] == 1):
+
+    if (arr[ti][tj] == 1):
             cnt += 1
-    elif(K == 1):
-        for z in range(1, K+1):
-            for si, sj in cross1:
-                if (checkLine(ti + si*z, tj + sj*z)):
-                    if(arr[ti + si*z][tj + sj*z] == 1):
-                        cnt += 1
+    if(K == 1):
+        for si, sj in cross1:
+            if (checkLine(ti + si*K, tj + sj*K)):
+                if(arr[ti + si*K][tj + sj*K] == 1):
+                    cnt += 1
     elif (K >= 2):
         # 십자
-        for z in range(1, K+1):
-            for si, sj in cross1:
-                if (checkLine(ti + si*z, tj + sj*z)):
-                    if(arr[ti + si*z][tj + sj*z] == 1):
-                        cnt += 1
+        for si, sj in cross1:
+            if (checkLine(ti + si*K, tj + sj*K)):
+                if(arr[ti + si*K][tj + sj*K] == 1):
+                    cnt += 1
         # 꺾인 십자
         for z in range(1, K):
-            for si, sj in cross2:
-                if (checkLine(ti + si*z, tj + sj*z)):
-                    if(arr[ti + si*z][tj + sj*z] == 1):
-                        cnt += 1
+            for si in range(ti -z, ti + z):
+                for sj in range(tj -z, tj + z):
+                    if (checkLine(ti + si*K, tj + sj*K)):
+                        if(arr[si][sj] == 1):
+                            cnt += 1
     return cnt
 
 def checkLine(i, j):
@@ -36,17 +35,13 @@ def checkLine(i, j):
 n, m = map(int, input().split()) # m은 골드 값, 한 칸단 한 cost
 arr = [list(map(int, input().split())) for _ in range(n)]
 cross1 = [[-1, 0], [0, 1], [1, 0], [0, -1]] # n -1까지
-cross2 = [[-1, -1], [-1, 1], [1, 1], [1, -1]] # n - 2까지
 cnt = 0
 for i in range(n):
     for j in range(n):
         for k in range(n):
             costSum = cost(k)
             tmp = calArea(k, i, j, n)
-            # if (i == 2 and j == 2 and k ==2):
-            #     print(tmp)
-            if ((tmp * m - costSum) > 0): # 손해가 아니면
+            if ((tmp * m - costSum) >= 0): # 손해가 아니면
                 if (cnt < tmp):
                     cnt = tmp
-                    # print(i, j, k)
 print(cnt)
