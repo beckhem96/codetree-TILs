e8ae1ea91c82fbc1efab4cdaaed4c772cf0b83b8
임완택 <57139287+beckhem96@users.noreply.github.com>
2024-02-18 15:46:05 +0900

    Title: 양수 직사각형의 최대 크기, Time: 187ms, Memory: 26MB, Status: Passed - CodeTree

--- 240218/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240218/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,6 +1,6 @@
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-ans = 0 # 양수 직사각형 중 가장 넓은 것
+ans = -1 # 양수 직사각형 중 가장 넓은 것
 
 def cal_width(x1, y1, x2, y2):
     return (x2 + 1 - x1) * (y2 + 1 - y1)
@@ -24,4 +24,5 @@ for i in range(n):
                 tmp = sum_rect(i, j, k, l)
                 if (tmp > 0):
                     ans = max(ans, cal_width(i, j, k, l))
+
 print(ans)
