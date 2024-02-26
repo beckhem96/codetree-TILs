4d8a666b6ebb3449f83ccecd9c82391ce2bc0831
임완택 <57139287+beckhem96@users.noreply.github.com>
2024-02-27 00:37:56 +0900

    Title: 최단 Run Length 인코딩, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240227/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,19 @@
+arr = list(input())
+n = len(arr)
+result = 20
+def shift():
+    tmp = arr[-1] 
+    for i in range(n-1, 0, -1):
+        arr[i] = arr[i-1]
+        arr[0] = tmp
+    return get_len()
+def get_len():
+    cnt = 1
+    for i in range(n-1):
+        if (arr[i] != arr[i+1]):
+            cnt += 1
+    return cnt * 2
+for _ in range(n):
+    result = min(shift(), result)
+
+print(result)
