0898e209f53884647ca9d03dd64b11d92628ba46
임완택 <57139287+beckhem96@users.noreply.github.com>
2024-03-10 21:05:07 +0900

    Title: 2차원 폭발 게임, Time: 1071ms, Memory: 31MB, Status: Passed - CodeTree

--- 240310/2차원 폭발 게임/The-2D-bomb-game.py
+++ 240310/2차원 폭발 게임/The-2D-bomb-game.py
@@ -1,65 +1,65 @@
-import copy
+BLANK = -1
+WILL_EXPLODE = 0
+
 n, m, k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-tmp_arr = [[0] *n for _ in range(n)] 
-next_arr = [[0] * n for _ in range(n)]
-result = 0
+tmp = [0 for _ in range(n)] 
 
-def bumb(i, j):
-    cnt = 0
-    tmp = []
-    for di in range(i, n):
-        if (arr[di][j] == arr[i][j]):
-            cnt += 1
-            tmp.append(di)
-        elif(arr[di][j] != arr[i][j]):
+def get_end_idx_of_explosion(start_idx, curr_num):
+    for end_idx in range(start_idx + 1, len(tmp)):
+        if tmp[end_idx] != curr_num:
+            return end_idx - 1
+    return len(tmp) - 1
+
+def explode():
+    while True:
+        did_explode = False
+        curr_idx = 0
+
+        while curr_idx < len(tmp):
+            end_idx = get_end_idx_of_explosion(curr_idx, tmp[curr_idx])
+
+            if end_idx - curr_idx + 1 >= m:
+
+                del tmp[curr_idx:end_idx + 1]
+                did_explode = True
+            else:
+                curr_idx = end_idx + 1
+        if not did_explode:
             break
-    if(cnt >= m):
-        for di in tmp:
-            arr[di][j] = 0 
 
-def move():
-   
-    flag = False
-    for j in range(n):
-        b_num = -1
-        for i in range(n-1, -1, -1):
-            # if (arr[i][j] != 0 and flag != True):
-            #     tmp_arr[i][j] = arr[i][j]
-            # elif (arr[i][j] != 0 and flag == True):
-            #     tmp_arr[b_num][j] = arr[i][j]
-            #     # b_num += 1
-            if (arr[i][j] == 0):
-                for k in range(i, -1, -1):
-                    if (arr[k][j] != 0):
-                        arr[i][j] = arr[k][j]
-                        arr[k][j] = 0
-                        break 
+def copy_column(col):
+    global tmp
+    tmp = [arr[row][col] for row in range(n) if arr[row][col] != BLANK]
+
+def copy_result(col):
+    for row in range(n - 1, -1, -1):
+        ## 원소가 비어있지 
+        arr[row][col] = tmp.pop() if tmp else BLANK
+
+def simulate():
+    for col in range(n):
+        copy_column(col)
+        explode()
+        copy_result(col)
 
 def rotate():
-    for i in range(n):
-        for j in range(n):
-            tmp_arr[i][j] = 0
-    
+    global arr
+
+    tmp_arr = [
+        [BLANK for _ in range(n)]
+        for _ in range(n)
+    ]
+
     for i in range(n):
         for j in range(n):
             tmp_arr[i][j] = arr[n - j - 1][i]
+    arr = tmp_arr
 
-    for i in range(n):
-        for j in range(n):
-            arr[i][j] = tmp_arr[i][j]
-            
-for _ in range(k):      
-    for i in range(n):
-        for j in range(n):
-            if (arr[i][j] != 0):
-                bumb(i, j)
-    move()
+simulate()
+for _ in range(k):
     rotate()
-    move()
+    simulate()
 
-for i in range(n):
-    for j in range(n):
-        if(arr[i][j] != 0):
-            result += 1
-print(result)
+answer = sum([arr[i][j] != BLANK for i in range(n) for j in range(n)])
+print(answer)
