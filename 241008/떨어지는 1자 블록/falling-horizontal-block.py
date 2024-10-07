e4bdce7584a80af0bfe0e6f695f7aca889f080ae
임완택 <57139287+beckhem96@users.noreply.github.com>
2024-10-08 00:46:41 +0900

    Title: 떨어지는 1자 블록, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 241008/떨어지는 1자 블록/falling-horizontal-block.py
+++ 241008/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,6 +1,7 @@
 n, m ,k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-dis, djs = [0, 1, 0], [-1, 0, 1]
+
+dis, djs = [1], [ 0]
 flag = False
 def check(i, j):
     for di, dj in zip(dis, djs):
@@ -10,15 +11,15 @@ def check(i, j):
     return False   
 for i in range(n):
     if (not flag):
-        for j in range(k-1, k+m):
+        for j in range(k-1, k+m-1):
             if (i == n-1):
                 arr[i][j] = 1
             else:
                 if (check(i, j)):
                     flag = True
                     break
-    else:
-        for j in range(k-1, k+m):
+    if (flag):
+        for j in range(k-1, k+m-1):
             arr[i][j] = 1
         break 
 
