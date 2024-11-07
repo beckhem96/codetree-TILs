f3a99dc5e4696672854d3c295652efd4869b7e91
임완택 <57139287+beckhem96@users.noreply.github.com>
2024-11-08 00:37:13 +0900

    Title: 2명의 도둑, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 241108/2명의 도둑/two-thieves.py
+++ 241108/2명의 도둑/two-thieves.py
@@ -1,6 +1,8 @@
+import copy
+
 n, m, c = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-first_thief = []
+first_theif = []
 max_nums = [[0 for _ in range(n)] for _ in range(n)]
 result = 0
     
@@ -41,14 +43,15 @@ def is_duplicate(i, x1, x2, x3, x4): # 시작점과 끝점
 
 for i in range(n):
     for j in range(n-m+1):
-        first_thief.append((i, j, j+m-1))
+        first_theif.append((i, j, j+m-1))
         max_nums[i][j] = add_nums(i, j, j+m-1)
 
-for i, j, k in first_thief:
-    for ni in range(n):
-        for nj in range(n-m+1):
-            if i == ni:
-                if is_duplicate(i, j, k, j, j+m-1):
-                    continue
-            result = max(result, max_nums[i][j] + max_nums[ni][nj])
+second_theif = copy.deepcopy(first_theif)
+
+for i, j, k in first_theif:
+    for ni, nj, nk in second_theif:
+        if i == ni:
+            if is_duplicate(i, j, k, nj, nk):
+                continue
+        result = max(result, max_nums[i][j] + max_nums[ni][nj])
 print(result)
